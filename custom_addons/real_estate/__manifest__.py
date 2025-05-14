36a44a86eaed10a0b527ade06b02a055a310274a
Mano ANDRIASATARINTSOA <159690397+ManoAndriasat@users.noreply.github.com>
2025-05-14 09:41:46 -0700

    chapter 5 : Some UI

---
+++ custom_addons/real_estate/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+
+    'summary': "my first module",
+
+    'description': """
+Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+
+'category': 'Sales',
+    'version': '0.1',
+
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
